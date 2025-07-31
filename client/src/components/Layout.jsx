40c5870dea10aba2ae32ddfb2973ce6ddfc6c395
arjuna-krishna-71 <arjunakrishna71@gmail.com>
2025-08-01 01:28:06 +0530

    Updating the UI using Tailwind/css

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 export const Layout = () => {
   return (
     <div>
         <Header></Header>
         <Outlet></Outlet>
+        <Footer></Footer>
             
     </div>
   )
